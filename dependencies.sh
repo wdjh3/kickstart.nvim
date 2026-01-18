2ea317d84a086491da6b170d87ddf9eaa30644af
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-01-18 13:57:41 +0800

    Add dependencies for WSL missing packages

--- dependencies.sh
+++ dependencies.sh
@@ -1,2 +1,7 @@
 # Needed for Telescope in NeoVim
 sudo apt install ripgrep fd-find
+
+# Might be needed for WSL
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+sudo apt install npm
+sudo apt install unzip
